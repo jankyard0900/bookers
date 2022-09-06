faa717ae82525d1b44a716377fd8d4264dd45db9
jankyard0900 <112614647+jankyard0900@users.noreply.github.com>
2022-09-06 09:00:10 +0000

    indexとshow,edit,destroyのリンク実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/'=>'homes#top'
   post 'books'=>'books#create'
   get 'books/index'=>'books#index'
-  get 'books/show'=>'books#show'
-  get 'books/edit'=>'books#edit'
+  get 'books/:id'=>'books#show',as: 'book'
+  get 'books/:id/edit'=>'books#edit',as: 'edit_book'
+  delete 'books/:id'=>'books#destroy',as: 'destroy_book'
 end
